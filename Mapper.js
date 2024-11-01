183a9f425e76899a1f616e132aed8ba249fa9b72
marco paternosto frosi <83019220+marcopf@users.noreply.github.com>
2024-11-01 09:56:42 +0100

    Update Mapper.js

--- Mapper.js
+++ Mapper.js
@@ -1,19 +1,28 @@
-let metadata: any = {}
+let metadata: any = {};
+
+interface ClassType<T> {
+    new (...args: any[]): T;
+}
+
+type MapperConfig = {
+    metadata: string;
+    class?: ClassType<any>;
+    callBackPre?: (self: any, originalValue: any) => void;
+    callBackPost?: (self: any, originalValue: any) => void;
+}
 
 export function findKey(obj: any, key: string): any | null {
-    if (obj.hasOwnProperty(key)) {
-        return obj[key];
-    }
+    let splitted = key.split('.');
+    let result:  any = JSON.parse(JSON.stringify(obj));
     
-    for (const k in obj) {
-        if (typeof obj[k] === 'object' && obj[k] !== null) {
-            const result = findKey(obj[k], key);
-            if (result !== null) {
-                return result; 
-            }
+    for (let step of splitted) {
+        if (result[step] !== undefined) {
+            result = result[step];
+        } else {
+            return null
         }
     }
-    return null;
+    return result;
 }
 
 export function updateValueByKey(obj: any, keyToFind: string, newValue: any): boolean {
@@ -39,36 +48,52 @@ export default function Mapper<T extends { new(...args: any[]): {} }>(constructo
     return class extends constructor {
         constructor(...args: any[]) {
             super(...args);
-          
+            
             Object.keys(this).forEach((key: any) => {
                 let storage;
-                if (metadata[key]){
+                //effettuo il reatrieve dell'oggetto da mappare
+                if (metadata[key])
                     storage = findKey(args[0], metadata[key]['metadata']);
-                    (this as any)[key] = storage;
-                }
                 else
                     storage = findKey(args[0], key);
+                //eseguo la callback pre
+                if (metadata[key]?.callBackPre)
+                    metadata[key]?.callBackPre(this, storage);
+
+                //se è un array di oggetti mappo ogni oggetto
                 if (Array.isArray(storage)){
                     let mapped  = [];
                     for(let el of storage){
-                        let instantiated = new metadata[key]['class'](el);
-                        mapped.push(instantiated);
+                        if (metadata[key]?.class){
+                            let instantiated = new metadata[key]['class'](el);
+                            
+                            mapped.push(instantiated);
+                        }
                     }
                     (this as any)[key] = mapped;
                 }
-                else if (typeof storage === 'object' && metadata[key]) {
-                    (this as any)[key] = new metadata[key]['class'](storage);
+                //se è un oggetto mappo l'oggetto
+                else if (typeof storage === 'object') {
+                    if (!metadata[key]?.class)
+                        (this as any)[key] = null;
+                    else
+                        (this as any)[key] = new metadata[key]['class'](storage);
                 }
+                //altrimenti mappo il valore
+                else{
+                    (this as any)[key] = storage;
+                }
+
+                //eseguo la callback post
+                if (metadata[key]?.callBackPost)
+                    metadata[key]?.callBackPost(this, storage);
             });
         }
     };
 }
 
-export function Map(metadataKey: string, classRef?: any) {
+export function Map(config?: MapperConfig) {
     return function(target: any, propertyKey: string) {
-        metadata[propertyKey] = {
-            metadata: metadataKey,
-            class: classRef
-        };
+        metadata[propertyKey] = config
     };
 }
